27aa15d751db5b654ec8fecd2369cd2cce946b4c
Matt Ware <mattbware3@gmail.com>
2023-11-13 20:17:47 +0000

    Setup player class

---
+++ Source/PlatformerJam/Private/Player/PJPlayer.cpp
@@ -0,0 +1,56 @@
+﻿// copyright lololol
+
+#include "Player/PJPlayer.h"
+
+#include "PJLog.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+APJPlayer::APJPlayer()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Boom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(Boom);
+	Boom->SetupAttachment(RootComponent);
+
+	Boom->bEnableCameraLag = true;
+	Boom->CameraLagSpeed = 3.0f;
+	
+    AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void APJPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APJPlayer::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	// APlayerController* const PlayerController = Cast<APlayerController>(NewController);
+	// if (!PlayerController)
+	// {
+	// 	UE_LOG(LogPJ, Warning, TEXT("PJPlayer possessed by non PlayerController!"));
+	// 	return;
+	// }
+	// PlayerController->SetViewTarget(this);
+}
+
+// Called every frame
+void APJPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APJPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/PlatformerJam/Public/Player/PJPlayer.h
@@ -0,0 +1,45 @@
+﻿// copyright lololol
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PJPlayer.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class PLATFORMERJAM_API APJPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APJPlayer();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE USpringArmComponent* GetBoom() { return Boom; }
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE UCameraComponent* GetCamera() { return Camera; }
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void PossessedBy(AController* NewController) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	USpringArmComponent* Boom;
+
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* Camera;
+};
