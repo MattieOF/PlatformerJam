afe1d9eaa1fac41c0739efc5b6814c14c0c38ee7
Matt Ware <mattbware3@gmail.com>
2023-11-15 22:12:31 +0000

    Added FCTween

--- Source/PlatformerJam/PlatformerJam.Build.cs
+++ Source/PlatformerJam/PlatformerJam.Build.cs
@@ -8,7 +8,7 @@ public class PlatformerJam : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "FCTween" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput" });
 
